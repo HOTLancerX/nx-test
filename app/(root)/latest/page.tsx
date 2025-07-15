1ef2ac61a8eecb94ef23803ac6eff54e1510176e
HeRa Khan <heraemt@gmail.com>
2025-07-15 20:46:56 +0600

    1

--- app/(root)/latest/page.tsx
+++ app/(root)/latest/page.tsx
@@ -1,8 +1,11 @@
+// app/latest/page.tsx
+import { Suspense } from 'react';
 import Latest from "@/components/Latest"
 
-export default function LatestPostsPage() {
-
+export default function LatestPage() {
   return (
-    <Latest />
-  )
+    <Suspense fallback={<div className="text-center py-10">Loading...</div>}>
+      <Latest />
+    </Suspense>
+  );
 }
